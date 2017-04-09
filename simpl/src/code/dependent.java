805646eb6385ddcdf9de06d5eee3ab909b9004bf
Vinoth Kumar Mahendira <vinothkumarmahendra@gmail.com>
2017-04-09 19:28:12 +0530

    merge code

--- simpl/src/code/dependent.java
+++ simpl/src/code/dependent.java
@@ -3,5 +3,8 @@ package code;
 public class dependent {
    public void print(){
 	   System.out.println("Hello from dependent");
+	   
+	   System.out.println("Hello from dependent1");
+	   System.out.println("Hello from dependent3");
    }
 }
