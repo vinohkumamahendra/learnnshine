b4fbb1050c52746b9ac221b9eabecdc67106293e
vinohkumamahendra <Vinothkumarmahendra@gmail.com>
2017-06-25 16:39:40 +0530

    Update dependent.java

--- simpl/src/code/dependent.java
+++ simpl/src/code/dependent.java
@@ -3,6 +3,7 @@ package code;
 public class dependent {
    public void print(){
 	   System.out.println("Hello from dependent");
+	   System.out.println("Hello from dependent");
 	   
 	   System.out.println("Hello from dependent1");
 	   System.out.println("Hello from dependent3");
